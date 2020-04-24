a6ce04aadbab994b40a711595e5facfadc7e67bb
王銘熙 <karta571110@gmail.com>
2020-04-24 20:03:52 +0800

    重弄金幣圖片

--- Assets/Scripts/TestMgr.cs
+++ Assets/Scripts/TestMgr.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class TestMgr : MonoBehaviour
 {
+
+    public GameObject prefab;
+    public Transform canvas;  // parent;
+
+
+    private Vector3 position = Vector3.zero;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        position.x = -300;
+        for (int i = 0; i < 10; i++)
+        {
+            GameObject newObject = Instantiate(prefab, canvas);
+            newObject.name = "coin_" + i;
+
+            //重新計算位置
+            position.x += 70; //step
+            newObject.GetComponent<Transform>().localPosition = position;
+        }
     }
 
     // Update is called once per frame
